dc17cdc8ed2927981e0fa34754875096be6a8370
Teresa Heidt <teresaheidt@hotmail.com>
2020-10-28 15:26:55 -0500

    deploying

--- server.js
+++ server.js
@@ -15,8 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/Transaction", 
+  {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
