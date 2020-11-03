6a53d456879d94a11212ce3b372349fb1f958eed
Teresa Heidt <teresaheidt@hotmail.com>
2020-11-02 18:06:39 -0600

    changed manifest to json

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/manifest.webmanifest",
+  "/manifest.json",
   "/style.css",
   "/icons/icon-152x152.png",
   "/icons/icon-192x192.png",
